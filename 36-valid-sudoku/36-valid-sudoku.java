da2c1a0de66ffa171a7d3ab522a5cf8bc1b5ac0b
Shaurya Kohli <shauryakohli17@gmail.com>
2022-06-06 22:55:23 +0530

    Time: 30 ms (10.32%), Space: 48.6 MB (12.64%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet <String> seen=new HashSet();
+        for(int i=0;i<9;i++)
+        {
+            for(int j=0;j<9;j++)
+            {
+                char current_val=board[i][j];
+                if(current_val!='.')
+                {
+                    if(!seen.add(current_val +"found in row"+i)||!seen.add(current_val +"found in column"+j)||!seen.add(current_val +"found in sub box"+i/3 +"."+j/3))
+                        return false;
+                }
+            }
+        }
+        return true;
+    }
+}
