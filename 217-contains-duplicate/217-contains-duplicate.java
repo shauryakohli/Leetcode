a5f0da9896f6a664bbc7da0019bdfbde7d471664
Shaurya Kohli <shauryakohli17@gmail.com>
2022-07-28 02:02:16 +0530

    Time: 6 ms (96.83%), Space: 54.7 MB (91.56%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -3,12 +3,12 @@ class Solution {
        Set<Integer>set=new HashSet<Integer>();
        for(int i=0;i<nums.length;i++)
        {
+           if(set.contains(nums[i]))
+              {
+                  return true;
+              }
            set.add(nums[i]);
-           if(set.size()!=i+1)
-           {
-               return true;
-           }
-           
+          
        }
         return false;
     }
