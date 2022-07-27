d4ce4fa087cb82d989304abb16e94d0093cf5e91
Shaurya Kohli <shauryakohli17@gmail.com>
2022-07-28 00:16:54 +0530

    Time: 8 ms (46.43%), Space: 43.5 MB (65.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -5,12 +5,9 @@ class Solution {
         int [] char_counts =new int[26];
         for(int i=0;i<s.length();i++)
         {
-            char_counts[s.charAt(i)-'a']++;
+            char_counts[s.charAt(i)-'a']++; 
+            char_counts[t.charAt(i)-'a']--;
         }
-       for(int i=0;i<t.length();i++)
-       {
-         char_counts[t.charAt(i)-'a']--;
-       }
         for(int count:char_counts)
         {
             if(count!=0)
