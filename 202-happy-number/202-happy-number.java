2d6a011da4bf96c71d675cf082be47deba1f7056
Shaurya Kohli <shauryakohli17@gmail.com>
2022-07-29 01:04:39 +0530

    Time: 218 ms (5.21%), Space: 48.6 MB (5.28%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,9 +1,7 @@
 class Solution {
-    public boolean isHappy(int n) {
-        if(Solution.getNumber(n) == 1) return true;
-        return false;
-    }
+  
      public static int getNumber(int n){
+         System.out.println(n);
          if(n<10){
             if(n==1) return 1;
             if(n==7) return 1;
@@ -11,10 +9,15 @@ class Solution {
         } 
         int sum=0, num =0;
          while(n!=0){
+            System.out.printf("Inside while : n = %d, num = %d\n",n,num);
             num = n % 10;
             sum = sum + (num * num);
             n = n / 10;
         }
        return getNumber(sum);
+    }
+      public boolean isHappy(int n) {
+        if(getNumber(n) == 1) return true;
+        return false;
     }
 }
