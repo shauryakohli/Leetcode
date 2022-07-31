57d31e4cb4ca3bd8d3d7ba826f234f5ebe16c60e
Shaurya Kohli <shauryakohli17@gmail.com>
2022-07-31 23:01:54 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (41.35%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -20,11 +20,10 @@ class Solution {
             return root;
         }
           invertTree(root.left);
+          invertTree(root.right);
           TreeNode temp=root.left;
-            // temp=root.left;
             root.left=root.right;
             root.right=temp;
-           invertTree(root.left);
             
         return root;
     }
