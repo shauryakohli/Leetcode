f9c07dbefa00e2a0bbaddce6d575f5cf567309df
Shaurya Kohli <shauryakohli17@gmail.com>
2022-08-06 13:45:11 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (72.80%) - LeetHub

---
+++ 458-poor-pigs/458-poor-pigs.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        int T=(minutesToTest/minutesToDie)+1;
+        int count=0;
+        int total=1;
+        while(total<buckets)
+        {
+            total=total*T;
+            count++;
+        }
+        return count;
+    }
+}
