7f39a19ad3649baad178c9253703ae20c8804c64
Shaurya Kohli <shauryakohli17@gmail.com>
2022-08-23 03:14:27 +0530

    Time: 1 ms (100.00%), Space: 41.2 MB (67.12%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+class Solution {
+    public boolean isPowerOfFour(int n) {
+     if(n==0)
+         return false;
+     if(n==1)
+         return true;
+     while(n%4==0)
+     {
+         n=n/4;
+     }
+        return n==1;
+    }
+}
